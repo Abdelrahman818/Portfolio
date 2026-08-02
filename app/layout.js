4093f5e1633c33350757e6e4df3683f533db6da2
Abdelrahman818 <bodigithub@gmail.com>
2026-08-02 12:56:01 +0300

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import CustomCursor from "@/components/CustomCursor";
+import { Analytics } from "@vercel/analytics/next"
 import "@/styles/globals.css";
 import "@/styles/responsive.css";
 
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body>
+        <Analytics />
         <CustomCursor />
         {children}
       </body>
